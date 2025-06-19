eb3e77004f3af57cf83f6bc0a70a3450e32f07d8
DATABASESK <server1.2148652652@gmail.com>
2025-06-19 18:37:52 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ5hQ08JQt0VYZCv_BRhnw_jC0rTAeeTE",
+  authDomain: "skyt-782c3.firebaseapp.com",
+  projectId: "skyt-782c3",
+  storageBucket: "skyt-782c3.firebasestorage.app",
+  messagingSenderId: "597345210065",
+  appId: "1:597345210065:web:03227bfa15114bcf3cbff0",
+  measurementId: "G-Z2NFZX18BQ"
+};
